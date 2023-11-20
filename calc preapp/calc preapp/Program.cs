c1c4ae9ee5eb3d8076c615fd4214dacffd2dafab
dobrodushniy <151165644+dobrodushniy@users.noreply.github.com>
2023-11-20 16:32:59 +0100

    20.11.23

--- calc preapp/calc preapp/Program.cs
+++ calc preapp/calc preapp/Program.cs
@@ -10,6 +10,93 @@ namespace calc_preapp
     {
         static void Main(string[] args)
         {
+            char again = 'y';
+            while (again == 'y')
+            {
+                double a;
+                double b;
+                double total;
+                char oper;
+
+                Console.WriteLine("enter the first number.");
+                if (!double.TryParse(Console.ReadLine(), out a))
+                {
+                    Console.WriteLine("enter a valid number.");
+                    continue;
+                }
+
+                Console.WriteLine("Enter operator (+, -, *, /):");
+                if (!char.TryParse(Console.ReadLine(), out oper) || !IsValidOperator(oper))
+                {
+                    Console.WriteLine("enter a valid operator (+, -, *, /).");
+                    continue;
+                }
+
+                Console.WriteLine("enter the second number.");
+                if (!double.TryParse(Console.ReadLine(), out b))
+                {
+                    Console.WriteLine("enter a valid number.");
+                    continue;
+                }
+
+                switch (oper)
+                    {
+                        case '+':
+                            total = a + b;
+                            Console.WriteLine($"{a} + {b} = {total}.");
+                            break;
+
+                        case '-':
+                            total = a - b;
+                            Console.WriteLine($"{a} - {b} = {total}.");
+                            break;
+
+                        case '*':
+                            total = a * b;
+                            Console.WriteLine($"{a} * {b} = {total}.");
+                            break;
+
+                        case '/':
+                            if (b == 0)
+                            {
+                                Console.WriteLine("cannot divide");
+                            }
+                            else
+                            {
+                                total = a / b;
+                                Console.WriteLine($"{a} / {b} = {total}.");
+                            }
+                            break;
+
+                        default:
+                            Console.WriteLine("Unknown operator.");
+                            break;
+                    }
+                
+                    Console.WriteLine("you want to continue? (y/n)");
+                again = Convert.ToChar(Console.ReadLine());
+
+
+            }
         }
+            static bool IsValidOperator(char op)
+            {
+                return op == '+' || op == '-' || op == '*' || op == '/';
+            }
+
+
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
